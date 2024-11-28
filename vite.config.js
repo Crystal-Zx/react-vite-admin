c8d4328c7b66ed36b52325b22d2061c4b7acdde2
zx <zhouxi@Mac-Studio.local>
2024-11-28 21:46:57 +0800

    build: :package: 新增 test 环境变量 demo, loadEnv 使用示例

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,8 @@ const envResolver = {
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
+  // 此处需要访问环境变量的话，请放开下面一行的注释，并从 vite 导入 loadEnv API
+  // const env = loadEnv(mode, process.cwd(), '')
+
   return envResolver[command]()
 })
